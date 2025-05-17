9e8b09a86630fd01e09245ba0580d73f34767898
MayurJ0001 <jadhavmayur77777@gmail.com>
2025-05-17 12:20:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.1"
+required_version= "~> 1.1"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,14 +10,12 @@ terraform {
  
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 access_key = var.access_key
 secret_key = var.secret_key
 }
 resource "aws_instance" "myec2" {
-  ami           = "ami-0e35ddab05955cf57"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "myinstance"
+  ami                     =  "ami-0e35ddab05955cf57"
+  instance_type           = "t2.micro"
 }
-}
+
